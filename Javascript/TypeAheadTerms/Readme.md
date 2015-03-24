6e16e03c1eec570dcf1d6d6bebc0bafd8f7c1f58
nemesek <dnemesek@gmail.com>
2015-03-24 10:09:19 -0500

    added some documentation

---
+++ Javascript/TypeAheadTerms/Readme.md
@@ -0,0 +1,3 @@
+This is an extension to the sample at http://blattchat.com/2013/01/09/bootstrap-typeahead/
+
+Basically we dynamically append the type ahead source when user hits the space key.  This allows the typeahead suggestions to continue on past the original source terms provided to it
--- Javascript/TypeAheadTerms/fnc.typeAheadTerms.js
+++ Javascript/TypeAheadTerms/fnc.typeAheadTerms.js
@@ -1,63 +1,79 @@
 var fncNS = fncNS || {};
 
-fncNS.typeAhead = (function($){
+fncNS.typeAhead = (function ($) {
     var $ = $;
-    var elem = '';
+    var jqSelector = '';
     var options = [];
+    var originalOptions = [];
 
 
-    function init(jqueryElement, terms){
+    function init(selector, terms) {
         // todo: add check
-        elem = jqueryElement;
+        jqSelector = selector;
         setTerms(terms);
         bindElementToTypeAhead();
         bindKeyPresses();
     }
 
-    function bindKeyPresses(){
+    function bindKeyPresses() {
 
-      $(elem).on('keypress',(function(e){
+        $(jqSelector).on('keypress', (function (e) {
 
-        if(e.which === 32){
-            // user pressed space
-            var text = $(elem).val();
-            updateAutoCompleteWithExistingText(text);
-        }
 
-      }));
+            if (e.which === 32) {
+                // user pressed space
+                var text = $(jqSelector).val();
+                updateAutoCompleteWithExistingText(text);
+            }
+
+            //if (e.which === 8) {
+            //    debugger;
+            //    // user backspaced
+            //    var text = $(jqSelector).val();
+            //    if (text.trim() === '') {
+            //        debugger;
+            //        // revert back to original options
+            //        options = originalOptions;
+            //        bindElementToTypeAhead();
+            //
+            //    }
+            //}
+
+        }));
     }
 
-    function bindElementToTypeAhead(){
-        //$('#product_search').typeahead().data('typeahead').source = options;
-        $(elem).typeahead().data('typeahead').source = options;
+    function bindElementToTypeAhead() {
+        $(jqSelector).typeahead().data('typeahead').source = options;
     }
 
-    function isFunctionLegit(fn){
+    function isFunctionLegit(fn) {
         return fn && typeof fn === 'function';
     }
 
-    function setTerms(terms){
-        if(isFunctionLegit((terms))){
+    function setTerms(terms) {
+        if (isFunctionLegit((terms))) {
             options = terms();
+            originalOptions = options;
             return;
         }
 
         // check if it is an array
-        if(terms.constructor === Array){
-          options = terms;
+        if (terms.constructor === Array) {
+            options = terms;
+            originalOptions = options;
+
         }
 
     }
 
     function updateAutoCompleteWithExistingText(text) {
         var terms = options;
-        var searchTerm = text.trim();
+
         if (text != '') {
             text = text + ' ';
         }
 
         for (var term in terms) {
-            debugger;
             options.push(text + terms[term]);
         }
 
@@ -69,87 +85,4 @@ fncNS.typeAhead = (function($){
     }
 })($);
 
-//$("#ordersSearchTextBox").on('keypress', (function (e) {
-//        if (e.which == 32) { // space
-//            // if user presses spaces and autocomplete is visible this will allow them to ignore autocomplete
-//            // and hit search without having to pick up the mouse
-//            if ($('.typeahead').is(':visible') && $('#ordersSearchTextBox').is(':focus')) {
-//                $('.typeahead').hide();
-//            } else if ($.trim(viewModel.fullTextOrderSearch().text()) != '') {
-//                // else if we have some text provided, fill autocomplete options
-//                if (viewModel.fullTextOrderSearch().text()) {
-//                    if (!viewModel.fullTextOrderSearch().hasSetStatusFromDropDown()) {
-//                        viewModel.fullTextOrderSearch().addStatusOptionsToAutoComplete();
-//                    } else {
-//                        viewModel.fullTextOrderSearch().repopulateAutoCompleteWithAllOptions();
-//                    }
-//                }
-//            } else {
-//                // if no text was provided, do not provided autocomplete options
-//                $('#ordersSearchTextBox').typeahead().data('typeahead').source = [];
-//            }
-//        }
-//        else if (e.which == 13) { // enter
-//
-//            // if the dropdown is not shown, search orders
-//            if (!$('.typeahead').is(':visible') && $('#ordersSearchTextBox').is(':focus')) {
-//                e.preventDefault();
-//                viewModel.fullTextOrderSearch().hasChanged(true);
-//                viewModel.searchOrders();
-//
-//            }
-//        }
-//        else if (e.which == 8) { // backspace
-//            viewModel.fullTextOrderSearch().repopulateAutoCompleteWithAllOptions();
-//        }
-//
-//        return e;
-//    }));
-//
-//
-//    self.repopulateAutoCompleteWithAllOptions = function (options) {
-//        debugger;
-//        var typeAheadOptions = [];
-//        if (options) {
-//            typeAheadOptions = options;
-//        }
-//
-//        $.when(self.getTypeAheadTerms()).then(function() {
-//            //var terms = self.getTypeAheadTerms();
-//
-//            var terms = self.typeAheadTerms;
-//            var text = self.text();
-//            text = text.trim();
-//            if (text != '') {
-//                text = text + ' ';
-//            }
-//
-//            for (var term in terms) {
-//                typeAheadOptions.push(text + terms[term]);
-//            }
-//
-//            $('#ordersSearchTextBox').typeahead().data('typeahead').source = typeAheadOptions; //self.getAllAutoCompleteOptions();
-//        });
-//
-//    }
-//
-//	    self.getTypeAheadTerms = function () {
-//
-//        var typeAheadTermsCompleted = $.Deferred();
-//        if (!self.hasTypeAheadTermsBeenCalled) {
-//            var url = '/Dashboard/GetTypeAheadTerms';
-//            var callData = { isServiceProviderView: viewModel.isServiceProviderView() ? 'TRUE' : 'FALSE' };
-//            var promise = viewModel.dataContext.get(url, callData);
-//            promise.done(function(data) {
-//                self.typeAheadTerms = data;
-//                self.hasTypeAheadTermsBeenCalled = true;
-//                typeAheadTermsCompleted.resolve();
-//            });
-//
-//
-//        } else {
-//            typeAheadTermsCompleted.resolve();
-//        }
-//
-//        return typeAheadTermsCompleted;
-//    };
+
