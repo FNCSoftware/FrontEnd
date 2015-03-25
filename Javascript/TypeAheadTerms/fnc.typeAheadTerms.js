0b96dfce1fc5483686138bda7b0f795c61248b58
Nemesek <dnemesek@gmail.com>
2015-03-24 23:43:10 -0500

    added ability to reset back to original source array, only tested on macbook and chrome

--- Javascript/TypeAheadTerms/fnc.typeAheadTerms.js
+++ Javascript/TypeAheadTerms/fnc.typeAheadTerms.js
@@ -1,7 +1,7 @@
 var fncNS = fncNS || {};
 
 fncNS.typeAhead = (function ($) {
-    var $ = $;
+    
     var jqSelector = '';
     var options = [];
     var originalOptions = [];
@@ -17,35 +17,64 @@ fncNS.typeAhead = (function ($) {
 
     function bindKeyPresses() {
 
+        // we could allow a fn arg to be passed in if we want to allow the
+        // client code to override the default functionality
+        // may require making other functions to be made public so for now we just do this
         $(jqSelector).on('keypress', (function (e) {
 
-            if (e.which === 32) {
+            var code = e.keyCode || e.which;
+
+            if (code === 32) {
                 // user pressed space
                 var text = $(jqSelector).val();
                 updateAutoCompleteWithExistingText(text);
+                return e;
             }
+
+        }));
+
+        $(jqSelector).on('keyup', (function(e) {
+
+            var code = e.keyCode || e.which;
+
+            if (code === 46 || code === 8) {
+
+                var text = $(jqSelector).val();
+
+                if(text.trim() === '') {
+                    bindElementToTypeAhead(originalOptions);
+                }
+            }
+
         }));
     }
 
-    function bindElementToTypeAhead() {
-        $(jqSelector).typeahead().data('typeahead').source = options;
+    function bindElementToTypeAhead(terms) { 
+
+        if(terms === undefined) {
+            terms = options;
+        }
+
+        $(jqSelector).typeahead().data('typeahead').source = terms;
     }
 
     function isFunctionLegit(fn) {
+
         return fn && typeof fn === 'function';
     }
 
     function setTerms(terms) {
+
         if (isFunctionLegit((terms))) {
             options = terms();
-            originalOptions = options;
+            originalOptions = options.slice();
             return;
         }
 
         // check if it is an array
         if (terms.constructor === Array) {
             options = terms;
-            originalOptions = options;
+            originalOptions = terms.slice();
 
         }
 
@@ -68,6 +97,6 @@ fncNS.typeAhead = (function ($) {
     return {
         init: init
     }
-})($);
+})(window.jQuery);
 
 
